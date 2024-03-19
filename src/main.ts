333d4a3f564cee2ed75f8ad750ca59dae12e3fd1
kone <70107937+konekowo@users.noreply.github.com>
2024-03-18 20:28:10 -0400

    add screens

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import { Application } from "pixi.js";
+import { Screen } from "./Screens/Screen";
+import { LoadScreen } from "./Screens/LoadScreen/LoadScreen";
+
+export class Main {
+    public static app: Application;
+    private static currentScreen: Screen | null;
+    private static allScreens: Screen[] = [];
+    public constructor(app: Application<HTMLCanvasElement>) {
+        Main.app = app;
+        // @ts-ignore
+        document.body.appendChild(Main.app.view);
+        Main.app.stage.eventMode = "dynamic";
+        this.doResize();
+        window.addEventListener("resize", this.doResize);
+        Main.showScreen(new LoadScreen());
+
+    }
+    public doResize(): void {
+        Main.app.renderer.resize(window.innerWidth, window.innerHeight);
+        Main.app.stage.scale.x = 1;
+        Main.app.stage.scale.y = 1;
+        Main.allScreens.forEach((screen) => {
+            screen.onResize();
+        })
+    }
+
+    public static showScreen(screen: Screen){
+        Main.app.stage.addChild(screen);
+        if (this.currentScreen != null){
+            this.currentScreen.onClose().then((lastScreen) => {
+                for (let i = 0; i < this.allScreens.length; i++) {
+                    if (this.allScreens[i] == lastScreen){
+                        this.allScreens.splice(i, 1);
+                    }
+                }
+                Main.app.ticker.remove(lastScreen.draw);
+                Main.app.stage.removeChild(lastScreen);
+                lastScreen.destroy();
+            });
+        }
+        screen.start();
+        this.allScreens.push(screen);
+        this.currentScreen = screen;
+        Main.app.ticker.add(screen.draw, screen);
+    }
+
+}
