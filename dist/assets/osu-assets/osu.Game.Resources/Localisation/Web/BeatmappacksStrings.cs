e8e7e85170acf555991bf713b95d85398d92772a
kone <70107937+konekowo@users.noreply.github.com>
2024-03-20 23:25:57 -0400

    some tweaks to the interaction screen

---
+++ dist/assets/osu-assets/osu.Game.Resources/Localisation/Web/BeatmappacksStrings.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Resources.Localisation.Web
+{
+    public static class BeatmappacksStrings
+    {
+        private const string prefix = @"osu.Game.Resources.Localisation.Web.Beatmappacks";
+
+        /// <summary>
+        /// "Pre-packaged collections of beatmaps based around a common theme."
+        /// </summary>
+        public static LocalisableString IndexDescription => new TranslatableString(getKey(@"index.description"), @"Pre-packaged collections of beatmaps based around a common theme.");
+
+        /// <summary>
+        /// "Coming soon!"
+        /// </summary>
+        public static LocalisableString IndexEmpty => new TranslatableString(getKey(@"index.empty"), @"Coming soon!");
+
+        /// <summary>
+        /// "listing"
+        /// </summary>
+        public static LocalisableString IndexNavTitle => new TranslatableString(getKey(@"index.nav_title"), @"listing");
+
+        /// <summary>
+        /// "Beatmap Packs"
+        /// </summary>
+        public static LocalisableString IndexTitle => new TranslatableString(getKey(@"index.title"), @"Beatmap Packs");
+
+        /// <summary>
+        /// "READ THIS BEFORE DOWNLOADING"
+        /// </summary>
+        public static LocalisableString IndexBlurbImportant => new TranslatableString(getKey(@"index.blurb.important"), @"READ THIS BEFORE DOWNLOADING");
+
+        /// <summary>
+        /// "Installation: Once a pack has been downloaded, extract the contents of the pack into your osu! Songs directory and osu! will do the rest."
+        /// </summary>
+        public static LocalisableString IndexBlurbInstallInstruction => new TranslatableString(getKey(@"index.blurb.install_instruction"), @"Installation: Once a pack has been downloaded, extract the contents of the pack into your osu! Songs directory and osu! will do the rest.");
+
+        /// <summary>
+        /// "Also note that it is highly recommended to {0}, since older maps are generally of much lower quality than more recent maps."
+        /// </summary>
+        public static LocalisableString IndexBlurbNoteDefault(LocalisableString scary) => new TranslatableString(getKey(@"index.blurb.note._"), @"Also note that it is highly recommended to {0}, since older maps are generally of much lower quality than more recent maps.", scary);
+
+        /// <summary>
+        /// "download the packs from latest to earliest"
+        /// </summary>
+        public static LocalisableString IndexBlurbNoteScary => new TranslatableString(getKey(@"index.blurb.note.scary"), @"download the packs from latest to earliest");
+
+        /// <summary>
+        /// "Download"
+        /// </summary>
+        public static LocalisableString ShowDownload => new TranslatableString(getKey(@"show.download"), @"Download");
+
+        /// <summary>
+        /// "cleared"
+        /// </summary>
+        public static LocalisableString ShowItemCleared => new TranslatableString(getKey(@"show.item.cleared"), @"cleared");
+
+        /// <summary>
+        /// "not cleared"
+        /// </summary>
+        public static LocalisableString ShowItemNotCleared => new TranslatableString(getKey(@"show.item.not_cleared"), @"not cleared");
+
+        /// <summary>
+        /// "{0} may not be used to clear this pack."
+        /// </summary>
+        public static LocalisableString ShowNoDiffReductionDefault(LocalisableString link) => new TranslatableString(getKey(@"show.no_diff_reduction._"), @"{0} may not be used to clear this pack.", link);
+
+        /// <summary>
+        /// "Difficulty reduction mods"
+        /// </summary>
+        public static LocalisableString ShowNoDiffReductionLink => new TranslatableString(getKey(@"show.no_diff_reduction.link"), @"Difficulty reduction mods");
+
+        /// <summary>
+        /// "Artist/Album"
+        /// </summary>
+        public static LocalisableString ModeArtist => new TranslatableString(getKey(@"mode.artist"), @"Artist/Album");
+
+        /// <summary>
+        /// "Spotlights"
+        /// </summary>
+        public static LocalisableString ModeChart => new TranslatableString(getKey(@"mode.chart"), @"Spotlights");
+
+        /// <summary>
+        /// "Featured Artist"
+        /// </summary>
+        public static LocalisableString ModeFeatured => new TranslatableString(getKey(@"mode.featured"), @"Featured Artist");
+
+        /// <summary>
+        /// "Project Loved"
+        /// </summary>
+        public static LocalisableString ModeLoved => new TranslatableString(getKey(@"mode.loved"), @"Project Loved");
+
+        /// <summary>
+        /// "Standard"
+        /// </summary>
+        public static LocalisableString ModeStandard => new TranslatableString(getKey(@"mode.standard"), @"Standard");
+
+        /// <summary>
+        /// "Theme"
+        /// </summary>
+        public static LocalisableString ModeTheme => new TranslatableString(getKey(@"mode.theme"), @"Theme");
+
+        /// <summary>
+        /// "Tournament"
+        /// </summary>
+        public static LocalisableString ModeTournament => new TranslatableString(getKey(@"mode.tournament"), @"Tournament");
+
+        /// <summary>
+        /// "You need to be {0} to download"
+        /// </summary>
+        public static LocalisableString RequireLoginDefault(LocalisableString link) => new TranslatableString(getKey(@"require_login._"), @"You need to be {0} to download", link);
+
+        /// <summary>
+        /// "signed in"
+        /// </summary>
+        public static LocalisableString RequireLoginLinkText => new TranslatableString(getKey(@"require_login.link_text"), @"signed in");
+
+        private static string getKey(string key) => $@"{prefix}:{key}";
+    }
+}
---
+++ dist/assets/osu-assets/osu.Game.Resources/Localisation/Web/SortStrings.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Resources.Localisation.Web
+{
+    public static class SortStrings
+    {
+        private const string prefix = @"osu.Game.Resources.Localisation.Web.Sort";
+
+        /// <summary>
+        /// "Sort by"
+        /// </summary>
+        public static LocalisableString Default => new TranslatableString(getKey(@"_"), @"Sort by");
+
+        /// <summary>
+        /// "All"
+        /// </summary>
+        public static LocalisableString All => new TranslatableString(getKey(@"all"), @"All");
+
+        /// <summary>
+        /// "Friends"
+        /// </summary>
+        public static LocalisableString Friends => new TranslatableString(getKey(@"friends"), @"Friends");
+
+        /// <summary>
+        /// "Recently active"
+        /// </summary>
+        public static LocalisableString LastVisit => new TranslatableString(getKey(@"last_visit"), @"Recently active");
+
+        /// <summary>
+        /// "Recent"
+        /// </summary>
+        public static LocalisableString New => new TranslatableString(getKey(@"new"), @"Recent");
+
+        /// <summary>
+        /// "Old"
+        /// </summary>
+        public static LocalisableString Old => new TranslatableString(getKey(@"old"), @"Old");
+
+        /// <summary>
+        /// "Rank"
+        /// </summary>
+        public static LocalisableString Rank => new TranslatableString(getKey(@"rank"), @"Rank");
+
+        /// <summary>
+        /// "Top"
+        /// </summary>
+        public static LocalisableString Top => new TranslatableString(getKey(@"top"), @"Top");
+
+        /// <summary>
+        /// "Username"
+        /// </summary>
+        public static LocalisableString Username => new TranslatableString(getKey(@"username"), @"Username");
+
+        /// <summary>
+        /// "Album"
+        /// </summary>
+        public static LocalisableString ArtistTracksAlbum => new TranslatableString(getKey(@"artist_tracks.album"), @"Album");
+
+        /// <summary>
+        /// "Artist"
+        /// </summary>
+        public static LocalisableString ArtistTracksArtist => new TranslatableString(getKey(@"artist_tracks.artist"), @"Artist");
+
+        /// <summary>
+        /// "BPM"
+        /// </summary>
+        public static LocalisableString ArtistTracksBpm => new TranslatableString(getKey(@"artist_tracks.bpm"), @"BPM");
+
+        /// <summary>
+        /// "Genre"
+        /// </summary>
+        public static LocalisableString ArtistTracksGenre => new TranslatableString(getKey(@"artist_tracks.genre"), @"Genre");
+
+        /// <summary>
+        /// "Length"
+        /// </summary>
+        public static LocalisableString ArtistTracksLength => new TranslatableString(getKey(@"artist_tracks.length"), @"Length");
+
+        /// <summary>
+        /// "Relevance"
+        /// </summary>
+        public static LocalisableString ArtistTracksRelevance => new TranslatableString(getKey(@"artist_tracks.relevance"), @"Relevance");
+
+        /// <summary>
+        /// "Title"
+        /// </summary>
+        public static LocalisableString ArtistTracksTitle => new TranslatableString(getKey(@"artist_tracks.title"), @"Title");
+
+        /// <summary>
+        /// "Update"
+        /// </summary>
+        public static LocalisableString ArtistTracksUpdate => new TranslatableString(getKey(@"artist_tracks.update"), @"Update");
+
+        /// <summary>
+        /// "Post time"
+        /// </summary>
+        public static LocalisableString ForumPostsCreated => new TranslatableString(getKey(@"forum_posts.created"), @"Post time");
+
+        /// <summary>
+        /// "Relevance"
+        /// </summary>
+        public static LocalisableString ForumPostsRelevance => new TranslatableString(getKey(@"forum_posts.relevance"), @"Relevance");
+
+        /// <summary>
+        /// "Created"
+        /// </summary>
+        public static LocalisableString ForumTopicsCreated => new TranslatableString(getKey(@"forum_topics.created"), @"Created");
+
+        /// <summary>
+        /// "Star priority"
+        /// </summary>
+        public static LocalisableString ForumTopicsFeatureVotes => new TranslatableString(getKey(@"forum_topics.feature_votes"), @"Star priority");
+
+        /// <summary>
+        /// "Last reply"
+        /// </summary>
+        public static LocalisableString ForumTopicsNew => new TranslatableString(getKey(@"forum_topics.new"), @"Last reply");
+
+        /// <summary>
+        /// "Relevance"
+        /// </summary>
+        public static LocalisableString UsersRelevance => new TranslatableString(getKey(@"users.relevance"), @"Relevance");
+
+        /// <summary>
+        /// "Name"
+        /// </summary>
+        public static LocalisableString UsersUsername => new TranslatableString(getKey(@"users.username"), @"Name");
+
+        private static string getKey(string key) => $@"{prefix}:{key}";
+    }
+}
