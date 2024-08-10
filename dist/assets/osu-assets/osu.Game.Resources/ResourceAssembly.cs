e618679adf7b53b0139027e74a4eb9cdbcfaf113
kone <70107937+konekowo@users.noreply.github.com>
2024-08-10 08:08:26 -0400

    ignore dist folder

--- dist/assets/osu-assets/osu.Game.Resources/ResourceAssembly.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-
-namespace osu.Game.Resources
-{
-    public static class OsuResources
-    {
-        public static Assembly ResourceAssembly => typeof(OsuResources).Assembly;
-    }
-}
